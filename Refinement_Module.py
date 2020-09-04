89dbcdea07e751d78ce5c8740decdcfecf6b5242
Kyle Weiher <kyleweiher@gmail.com>
2020-09-04 14:13:48 +0200

    Remove blade layer norm since it doesn't work.

--- Refinement_Module.py
+++ Refinement_Module.py
@@ -1,30 +1,6 @@
 import torch
 import torch.nn as nn
 
-
-class LayerNorm(nn.Module):
-    def __init__(self, num_features, eps=1e-12, affine=True):
-        super(LayerNorm, self).__init__()
-        self.num_features = num_features
-        self.affine = affine
-        self.eps = eps
-
-        if self.affine:
-            self.gamma = nn.Parameter(torch.ones(num_features))
-            self.beta = nn.Parameter(torch.zeros(num_features))
-
-    def forward(self, x):
-        shape = [-1] + [1] * (x.dim() - 1)
-        mean = x.view(x.size(0), -1).mean(1).view(*shape)
-        std = x.view(x.size(0), -1).std(1).view(*shape)
-
-        y = (x - mean) / (std + self.eps)
-        if self.affine:
-            shape = [1, -1] + [1] * (x.dim() - 2)
-            y = self.gamma.view(*shape) * y + self.beta.view(*shape)
-        return y
-
-
 class RefinementModule(nn.Module):
     r"""
     One 3 layer module making up a segment of a CRN. Mask input tensor & prior layers get resized.
@@ -92,8 +68,6 @@ class RefinementModule(nn.Module):
             )
         elif norm_type == "half":
             self.norm_1 = nn.LayerNorm(torch.Size(input_height_width))
-        elif norm_type == "blade":
-            self.norm_1 = LayerNorm(self.true_inner_channel_count)
         elif norm_type == "none":
             self.norm_1 = lambda x: x
         elif norm_type == "group":
@@ -129,8 +103,6 @@ class RefinementModule(nn.Module):
             )
         elif norm_type == "half":
             self.norm_2 = nn.LayerNorm(torch.Size(input_height_width))
-        elif norm_type == "blade":
-            self.norm_2 = LayerNorm(self.true_inner_channel_count)
         elif norm_type == "none":
             self.norm_2 = lambda x: x
         elif norm_type == "group":
