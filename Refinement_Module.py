482b5dcd9369b27f9ef34b332cd8798feffc8198
Kyle Weiher <kyleweiher@gmail.com>
2020-10-17 16:18:52 +0200

    Refactor Refinement_Module.py to be more concise.

--- Refinement_Module.py
+++ Refinement_Module.py
@@ -2,6 +2,58 @@ import torch
 import torch.nn as nn
 
 
+class RMBlock(nn.Module):
+    def __init__(
+        self,
+        input_height_width: tuple,
+        input_channel_count: int,
+        output_channel_count: int,
+        norm_type: str,
+    ):
+        super().__init__()
+        # Module architecture
+        self.conv_1 = nn.Conv2d(
+            input_channel_count,
+            output_channel_count,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        RefinementModule.init_conv_weights(self.conv_1)
+
+        if norm_type == "full":
+            self.norm_1 = nn.LayerNorm(
+                RefinementModule.change_output_channel_size(
+                    input_height_width, output_channel_count
+                ),
+            )
+        elif norm_type == "half":
+            self.norm_1 = nn.LayerNorm(torch.Size(input_height_width))
+        elif norm_type == "none":
+            self.norm_1 = lambda x: x
+        elif norm_type == "group":
+            self.norm_1 = nn.GroupNorm(1, output_channel_count)
+        elif norm_type == "instance":
+            self.norm_1 = nn.InstanceNorm2d(
+                output_channel_count,
+                eps=1e-05,
+                momentum=0.1,
+                affine=False,
+                track_running_stats=False,
+            )
+        elif norm_type == "weight":
+            self.norm_1 = lambda x: x
+            self.conv_1 = nn.utils.weight_norm(self.conv_1)
+
+        self.leakyReLU_1 = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_1(x)
+        x = self.norm_1(x)
+        x = self.leakyReLU_1(x)
+        return x
+
+
 class RefinementModule(nn.Module):
     def __init__(
         self,
@@ -14,7 +66,7 @@ class RefinementModule(nn.Module):
         input_height_width: tuple,
         norm_type: str,
     ):
-        super(RefinementModule, self).__init__()
+        super().__init__()
 
         self.input_height_width: tuple = input_height_width
 
@@ -31,78 +83,20 @@ class RefinementModule(nn.Module):
         self.final_conv_output_channel_count: int = final_conv_output_channel_count
         self.use_feature_encoder: int = feature_encoder_input_channel_count > 0
         self.is_final_module: bool = self.final_conv_output_channel_count > 0
-        # self.dropout = nn.Dropout2d(p=0.1)
 
-        # Module architecture
-        self.conv_1 = nn.Conv2d(
+        self.rm_block_1 = RMBlock(
+            self.input_height_width,
             self.total_input_channel_count,
             self.base_conv_channel_count,
-            kernel_size=3,
-            stride=1,
-            padding=1,
+            norm_type
         )
-        RefinementModule.init_conv_weights(self.conv_1)
 
-        if norm_type == "full":
-            self.norm_1 = nn.LayerNorm(
-                RefinementModule.change_output_channel_size(
-                    input_height_width, self.base_conv_channel_count
-                ),
-            )
-        elif norm_type == "half":
-            self.norm_1 = nn.LayerNorm(torch.Size(input_height_width))
-        elif norm_type == "none":
-            self.norm_1 = lambda x: x
-        elif norm_type == "group":
-            self.norm_1 = nn.GroupNorm(1, self.base_conv_channel_count)
-        elif norm_type == "instance":
-            self.norm_1 = nn.InstanceNorm2d(
-                self.base_conv_channel_count,
-                eps=1e-05,
-                momentum=0.1,
-                affine=False,
-                track_running_stats=False,
-            )
-        elif norm_type == "weight":
-            self.norm_1 = lambda x: x
-            self.conv_1 = nn.utils.weight_norm(self.conv_1)
-
-        self.leakyReLU_1 = nn.LeakyReLU(negative_slope=0.2, inplace=True)
-
-        self.conv_2 = nn.Conv2d(
+        self.rm_block_2 = RMBlock(
+            self.input_height_width,
             self.base_conv_channel_count,
             self.base_conv_channel_count,
-            kernel_size=3,
-            stride=1,
-            padding=1,
+            norm_type
         )
-        RefinementModule.init_conv_weights(self.conv_2)
-
-        if norm_type == "full":
-            self.norm_2 = nn.LayerNorm(
-                RefinementModule.change_output_channel_size(
-                    input_height_width, self.base_conv_channel_count
-                ),
-            )
-        elif norm_type == "half":
-            self.norm_2 = nn.LayerNorm(torch.Size(input_height_width))
-        elif norm_type == "none":
-            self.norm_2 = lambda x: x
-        elif norm_type == "group":
-            self.norm_2 = nn.GroupNorm(1, self.base_conv_channel_count)
-        elif norm_type == "instance":
-            self.norm_2 = nn.InstanceNorm2d(
-                self.base_conv_channel_count,
-                eps=1e-05,
-                momentum=0.1,
-                affine=False,
-                track_running_stats=False,
-            )
-        elif norm_type == "weight":
-            self.norm_2 = lambda x: x
-            self.conv_2 = nn.utils.weight_norm(self.conv_2)
-
-        self.leakyReLU_2 = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
         if self.is_final_module:
             self.final_conv = nn.Conv2d(
@@ -134,7 +128,7 @@ class RefinementModule(nn.Module):
         mask: torch.Tensor,
         prior_layers: torch.Tensor,
         feature_selection: torch.Tensor,
-        edge_map: torch.Tensor
+        edge_map: torch.Tensor,
     ):
 
         # Downsample mask for current RM
@@ -162,13 +156,8 @@ class RefinementModule(nn.Module):
             )
             x = torch.cat((x, feature_selection, edge_map), dim=1)
 
-        x = self.conv_1(x)
-        x = self.norm_1(x)
-        x = self.leakyReLU_1(x)
-
-        x = self.conv_2(x)
-        x = self.norm_2(x)
-        x = self.leakyReLU_2(x)
+        x = self.rm_block_1(x)
+        x = self.rm_block_2(x)
 
         if self.is_final_module:
             x = self.final_conv(x)
